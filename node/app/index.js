e240e881a1a8459b0dd2dff011648cb9baa43d3e
Hai Luu <hai.luu@advesa.com>
2020-12-14 11:55:54 +0700

    update nodejs server

--- node/app/index.js
+++ node/app/index.js
@@ -9,32 +9,34 @@ const sharp = require('sharp');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// Image convert quality + resize
-const QUATLITY = 1;
-const RESIZE = 300;
+// Image resize
+const RESIZE = 100;
 
 const app = express();
 app.use(fileUpload());
 
 app.post('/heic', async (req, res) => {
     if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(400).send('No files were uploaded.');
+        return res.status(400).json({ 'message': 'No files were uploaded.' });
     }
 
     let img = req.files.img;
 
     const outputBuffer = await convert({
         buffer: img.data,
-        format: 'JPEG',
-        quality: QUATLITY
+        format: 'PNG'
     });
 
     await sharp(outputBuffer)
         .resize(RESIZE)
         .toBuffer()
         .then(output => {
-            const base64Image = output.toString('base64');
-            res.json({ "data": `data:image/jpeg;base64,${base64Image}` });
+            res.writeHead(200, {
+                'Content-Type': 'image/png',
+                'Content-Length': output.length
+            });
+
+            res.end(output);
         })
 });
 
